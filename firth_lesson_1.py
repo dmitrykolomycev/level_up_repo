c9a72c54c8114aacda70a838c4e7c7bbaa5a89f4
dmitrykolomycev <dmitry.kolomycev@gmail.com>
2024-12-19 20:54:53 +0300

    4 урок, 6 заданий первых

---
+++ firth_lesson_1.py
@@ -0,0 +1,9 @@
+a = int(input("first value: "))
+b = int(input("second value число: "))
+if a > b:
+    print("first value bigger: " + str(a))
+elif a  == b:
+    print("value  equality" )
+else:
+    print("second value bigger: " + str(b))
+
