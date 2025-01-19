92f857609653a3b4e76b950b0e7987a5be91eaee
dmitrykolomycev <80475144+dmitrykolomycev@users.noreply.github.com>
2025-01-20 00:16:53 +0300

    Update task8.py

--- HomeWork3/task8.py
+++ HomeWork3/task8.py
@@ -12,10 +12,3 @@ filter = [f for f in greetings if "Скучный" not in f]
 congratilations = random.choice(filter)
 
 print(congratilations)
-"""
-greetings = ["С днём рождения!", "С Новым годом!", "Желаю успехов!", "Скучный текст"]
-filtered_greetings = [g for g in greetings if "Cкучный" not in g]
-
-random_greeting = random.choice(filtered_greetings)
-
-print("Случайное поздравление:", random_greeting)"""
